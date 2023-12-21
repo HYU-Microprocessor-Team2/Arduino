71b21170c642f53d05f5bd5f1e718528d17bdef3
HyunJungWoo <149641822+pigeonbabysparrow@users.noreply.github.com>
2023-12-21 14:14:11 +0900

    Update main_slave.ino

--- main_slave/main_slave.ino
+++ main_slave/main_slave.ino
@@ -1,12 +1,15 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial btSerial(2,3); // RX, TX
+SoftwareSerial btSerial(2,3); // TX, RX
 
 bool direction_flag[2];
+int motor_L = 8; // Left motor
+int motor_R = 9; // Right motor
 
 void setup() {
-  btSerial.begin(9600); // HC-05 typically communicates at 9600 baud rate.
-  DDRD = 0x03; // Set pin 8 and 9 as output mode.
+  btSerial.begin(38400); // HC-05 typically communicates at 9600 baud rate.
+  pinMode(motor_L, OUTPUT); // Set pin 8 as output mode.
+  pinMode(motor_R, OUTPUT); // Set pin 9 as output mode.
 }
 
 void loop() {
@@ -16,19 +19,33 @@ void loop() {
       direction_flag[i] = btSerial.read(); // Use the read() function to get the data
     }
 
+    int speed = 255 * 70; // maxmimum * 0.70
+
     // Power the pin according to the received data.
-    // Connect 8pin to Left vibration. connect 9pin to Right vibration.
+    // Connect 8pin to Left motor. connect 9pin to Right motor.
     if(direction_flag[0] == 1 && direction_flag[1] == 0) {
-      digitalWrite(8, HIGH); // Power the pin 8.
-      digitalWrite(9, LOW); // Do not power the pin 9. Go to Left
+      analogWrite(motor_L, speed); // Power the pin 8.
+      analogWrite(motor_R, LOW); // Do not power the pin 9. Go to Left.
+      delay(1000);
+      analogWrite(motor_L, LOW); // Turn off the pin 8.
+      analogWrite(motor_R, LOW); // Turn off the pin 9.
+      delay(1000); // Wait for 1 second before the next vibration.
     }
     else if(direction_flag[0] == 0 && direction_flag[1] == 1) {
-      digitalWrite(8, LOW); // Do not power the pin 8.
-      digitalWrite(9, HIGH); // Power the pin 9. Go to Right
+      analogWrite(motor_L, LOW); // Do not power the pin 8.
+      analogWrite(motor_R, speed); // Power the pin 9. Go to Right.
+      delay(1000);
+      analogWrite(motor_L, LOW); // Turn off the pin 8.
+      analogWrite(motor_R, LOW); // Turn off the pin 9.
+      delay(1000); // Wait for 1 second before the next vibration.
     }
     else if(direction_flag[0] == 1 && direction_flag[1] == 1) {
-      digitalWrite(8, HIGH); // power the pin 8.
-      digitalWrite(9, HIGH); // power the pin 9. Go to striaght
+      analogWrite(motor_L, speed); // power the pin 8.
+      analogWrite(motor_R, speed); // power the pin 9. Go to straight.
+      delay(1000);
+      analogWrite(motor_L, LOW); // Turn off the pin 8.
+      analogWrite(motor_R, LOW); // Turn off the pin 9.
+      delay(1000); // Wait for 1 second before the next vibration.
     }
   }
 }
